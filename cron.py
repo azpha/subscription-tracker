e64bffc78fa54c4e1eb71cc802052c05235c1c4f
Alex Frantz <alex@alexav.gg>
2025-03-21 01:50:58 -0400

    add bump job back

--- cron.py
+++ cron.py
@@ -17,11 +17,15 @@ def job():
 
         if len(subscriptions) > 0:
             fields = []
+            expired_subscriptions = []
 
             for subscription in subscriptions:
                 current_date = date.today()
                 next_date = dateutil.parser.isoparse(subscription["nextBillingDate"]).date()
                 difference = next_date - current_date
+
+                if difference.days <= 0:
+                    expired_subscriptions.append(subscription)
 
                 fields.append(
                     {
@@ -42,6 +46,10 @@ def job():
                     }
                 ]
             })
+
+            # run job to bump expired subs
+            bump_expired_subs(expired_subscriptions)
+
     except Exception as e:
         print("Failed to fetch subscriptions")
         send_discord_webhook_message({
@@ -59,10 +67,34 @@ def job():
 def send_discord_webhook_message(embed):
     requests.post(discord_webhook_url, json=embed, headers=discord_webhook_headers)
 
+def bump_expired_subs(expired_subscriptions):
+    for subscription in expired_subscriptions:
+        current_expiry_date = dateutil.parser.isoparse(subscription["nextBillingDate"])
+        current_expiry_date = current_expiry_date.replace(month=current_expiry_date.month + 1)
+        current_expiry_date = current_expiry_date.strftime("%Y-%m-%dT%H:%M:%S.000Z")
+
+        body = {
+            "0": {
+                "json": {
+                    "id": subscription["id"],
+                    "nextBillingDate": current_expiry_date
+                },
+                "meta": {
+                    "values": {
+                        "nextBillingDate": ["Date"]
+                    }
+                }
+            }
+        }
+
+        r = requests.post(f"{url}/api/trpc/subs.update?batch=1", json=body, headers={'content-type': 'application/json'})
+        r.raise_for_status()
+
 def main():
     schedule.every().day.do(job)
     # schedule.every(5).seconds.do(job)
 
+    job() # run for cleanup
     while True:
         schedule.run_pending()
 
